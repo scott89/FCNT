f16f1e1c4e5ae5f7e5ba980c672dbcb7f39ca788
sott89 <scott89@github.com>
2016-08-29 09:56:39 +0800

    misc update

--- run.m
+++
@@ -1,7 +0,0 @@
-addpath('caffe-fcnt/matlab/caffe/', 'util');
-data_path = ['video/'];
-seq_name = 'MotorRolling';
-track_param = init_tracker(data_path, seq_name);
-position = cnn2_pf_tracker(track_param);
-caffe('reset');
-
