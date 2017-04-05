d2d9e72b13d2c0c1f5d13fdd40854be2a19aa188
sott89 <scott89@github.com>
2017-04-05 15:35:13 +0800

    add video_download script

---
+++ video/download_motorrolling.sh
@@ -0,0 +1,6 @@
+#! /bin/sh 
+cd ./video
+wget http://cvlab.hanyang.ac.kr/tracker_benchmark/seq/motorRolling.zip
+unzip motorRolling.zip #-d tmp
+#mv tmp/Basketball/img Basketball 
+rm motorRolling.zip
